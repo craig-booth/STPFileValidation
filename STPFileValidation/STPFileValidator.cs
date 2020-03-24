50ba4f791b316dfd5e8746526f36d93b962e31f1
Craig Booth <craigb@neller.com.au>
2020-03-24 16:12:52 +1030

    Improve handling of invalid xml documents

--- STPFileValidation/STPFileValidator.cs
+++ STPFileValidation/STPFileValidator.cs
@@ -20,6 +20,8 @@ namespace STPFileValidation
     {
         private STPFile _STPFile;
         private bool _XSDError;
+        private string _NodeName;
+        private string _PayrollId;
 
         private List<ValidationError> _Errors = new List<ValidationError>();
         public IEnumerable<ValidationError> Errors
@@ -68,11 +70,18 @@ namespace STPFileValidation
         {
             var successfull = true;
 
-            stream.Position = 0;
-            successfull &= ValidatePAYEVNTStructure(stream);
+            try
+            {
+                stream.Position = 0;
+                successfull &= ValidatePAYEVNTStructure(stream);
 
-            stream.Position = 0;
-            successfull &= ValidatePAYEVNTRules(stream);
+                stream.Position = 0;
+                successfull &= ValidatePAYEVNTRules(stream);
+            }
+            catch
+            {
+                return false;
+            }
 
             return successfull;
         }
@@ -117,11 +126,18 @@ namespace STPFileValidation
         {
             var successfull = true;
 
-            stream.Position = 0;
-            successfull &= ValidatePAYEVNTEMPStructure(stream);
+            try
+            {
+                stream.Position = 0;
+                successfull &= ValidatePAYEVNTEMPStructure(stream);
 
-            stream.Position = 0;
-            successfull &= ValidatePAYEVNTEMPRules(stream);
+                stream.Position = 0;
+                successfull &= ValidatePAYEVNTEMPRules(stream);
+            }
+            catch
+            {
+                return false;
+            }
 
             return successfull;
         }
@@ -142,6 +158,8 @@ namespace STPFileValidation
                 {
                     DocumentId = _STPFile.DocumentId,
                     DocumentType = _STPFile.DocumentType,
+                    IdNumber = "",
+                    TransferNumber = "",
                     ErrorCode = message.Code,
                     Description = message.Description,
                     LongDescription = message.LongDescription,
@@ -153,6 +171,12 @@ namespace STPFileValidation
                     error.Severity = Severity.Information;
                 else
                     error.Severity = Severity.Error;
+
+                if (_PayrollId != "")
+                {
+                    error.IdNumber = _PayrollId.Substring(1, _PayrollId.Length - 1);
+                    error.TransferNumber = _PayrollId.Substring(_PayrollId.Length, 1);
+                }
 
                 _Errors.Add(error);
                 successfull = false;
@@ -174,14 +198,33 @@ namespace STPFileValidation
         private bool ValidateAgainstXSD(Stream stream, string nameSpace, string xsdFile)
         {
             _XSDError = false;
+            _NodeName = "";
+            _PayrollId = "";
+
+            var settings = new XmlReaderSettings();
+            settings.ValidationType = ValidationType.Schema;
+            settings.Schemas.Add(nameSpace, xsdFile);
+            settings.ValidationEventHandler += ValidationEventHandler;
+
+            var xmlReader = XmlReader.Create(stream, settings);
+
             try
             {
-                var xmlReader = XmlReader.Create(stream);
-                var xmlDocument = XDocument.Load(xmlReader);
+                while (xmlReader.Read())
+                {
+                    if (xmlReader.NodeType == XmlNodeType.Element)
+                    {
+                        _NodeName = xmlReader.LocalName;
+                    }
+                    else if (xmlReader.NodeType == XmlNodeType.Text)
+                    {
+                        if (_NodeName == "EmploymentPayrollNumberId")
+                        {
+                            _PayrollId = xmlReader.Value;
+                        }
+                    }
+                }
 
-                var xmlSchema = new XmlSchemaSet();
-                xmlSchema.Add(nameSpace, xsdFile);
-                xmlDocument.Validate(xmlSchema, ValidationEventHandler);
             }
             catch (Exception e)
             {
@@ -192,16 +235,26 @@ namespace STPFileValidation
                     ErrorCode = "Invalid XML Document",
                     Description = e.Message,
                     LongDescription = "",
-                    Location = "",
+                    Location = _NodeName,
                     Severity = Severity.Error
                 };
+                if (_PayrollId != "")
+                {
+                    error.IdNumber = _PayrollId.Substring(0, _PayrollId.Length - 1);
+                    error.TransferNumber = _PayrollId.Substring(_PayrollId.Length - 1, 1);
+                }
+                else
+                {
+                    error.IdNumber = "";
+                    error.TransferNumber = "";
+                }
 
                 _Errors.Add(error);
-                _XSDError = true;
+
+                throw new XmlException();
             }
 
-
-            return _XSDError;
+            return !_XSDError;
         }
 
         private void ValidationEventHandler(object sender, ValidationEventArgs e)
@@ -217,6 +270,17 @@ namespace STPFileValidation
                 Severity = Severity.Error
             };
 
+            if (_PayrollId != "")
+            {
+                error.IdNumber = _PayrollId.Substring(0, _PayrollId.Length - 1);
+                error.TransferNumber = _PayrollId.Substring(_PayrollId.Length - 1, 1);
+            }
+            else
+            {
+                error.IdNumber = "";
+                error.TransferNumber = "";
+            }
+
             _Errors.Add(error);
             _XSDError = true;
         }
@@ -226,11 +290,24 @@ namespace STPFileValidation
     public class ValidationError
     {
         public string DocumentId { get; set; }
-        public string DocumentType { get; set; }
+        public string DocumentType { get; set; }  
+        public string IdNumber { get; set; }
+        public string TransferNumber { get; set; }
         public Severity Severity { get; set; }
         public string ErrorCode { get; set; }
         public string Description { get; set; }
         public string LongDescription { get; set; }
         public string Location { get; set; }
+
+        public string ErrorLocation
+        {
+            get
+            {
+                if (DocumentType == "PAYEVNTEMP")
+                    return "Employee " + IdNumber + ", Transfer " + TransferNumber + " (Document " + DocumentId + ") : " + Location;
+                else
+                    return "Employer Information (Document " + DocumentId + ") : " + Location;
+            }
+        }
     }
 }
